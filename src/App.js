bea4104f4c5e3083c09cda1d73498ff8a9b6aae0
Karastan Vladislav <90850632+Kar1stan@users.noreply.github.com>
2023-04-26 17:39:54 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Home from "./page/Home";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
